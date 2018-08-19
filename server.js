4e787497273f724b1d4eb2a40996a7bdd898ff40
Oyedeji Peace <oyedejipeace@gmail.com>
2018-08-19 10:04:13 -0800

    Added linting library,style guide  and testing framework

--- server.js
+++ server.js
@@ -4,5 +4,15 @@ apt.use(express.static(__dirname+'/UI'))
 apt.get('/',(req,res) => {
     res.sendFile(__dirname+'/UI/index.html');
 });
-apt.get
+apt.get('/api/v1/questions/',(req,res) => {
+    res.send('Hello World');
+});
+
+var fs = require('fs');
+fs.writeFile("test.txt", jsonData, function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
 apt.listen(8080);
+
