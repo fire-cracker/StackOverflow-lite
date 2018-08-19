ba39662930cd2272f6dbe5df56e66e68cfb722f2
Oyedeji Peace <oyedejipeace@gmail.com>
2018-08-19 10:26:08 -0800

    Revert "Added linting library,style guide  and testing framework"
    This reverts commit 4e787497273f724b1d4eb2a40996a7bdd898ff40.

--- server.js
+++ server.js
@@ -4,15 +4,5 @@ apt.use(express.static(__dirname+'/UI'))
 apt.get('/',(req,res) => {
     res.sendFile(__dirname+'/UI/index.html');
 });
-apt.get('/api/v1/questions/',(req,res) => {
-    res.send('Hello World');
-});
-
-var fs = require('fs');
-fs.writeFile("test.txt", jsonData, function(err) {
-    if (err) {
-        console.log(err);
-    }
-});
+apt.get
 apt.listen(8080);
-
