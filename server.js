1853bc3b6bb3ecb50496fe6fea96d3541794c811
Oyedeji Peace <oyedejipeace@gmail.com>
2018-08-21 20:35:18 -0800

    feat (Api endpoints)
    add gitignore to ignore node_modules and heroku folder
    -procfile to tell heroku what server it will be running and what commands it needs to run in other to launch the application
    - app.json which descriibes my application to heroku
    [Finishes #159781566]

--- server.js
+++
@@ -1,8 +0,0 @@
-const express= require("express");
-const apt=express();
-apt.use(express.static(__dirname+'/UI'))
-apt.get('/',(req,res) => {
-    res.sendFile(__dirname+'/UI/index.html');
-});
-apt.get
-apt.listen(8080);
